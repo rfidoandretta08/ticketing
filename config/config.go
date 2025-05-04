3e35bd16a5a1d4b2b9e5babe03a9ec5d776c14a9
mac <mac@macs-MacBook-Air.local>
2025-05-04 10:14:17 +0700

    initial commit

--- config/config.go
+++ config/config.go
@@ -1,15 +1,10 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"ticketing/model" // Sesuaikan dengan path model Anda
-
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 type Config struct {
@@ -36,29 +31,4 @@ func LoadConfig() *Config {
 		DBName:     os.Getenv("DB_NAME"),
 		JWTSecret:  os.Getenv("JWT_SECRET"),
 	}
-}
-
-func SetupDatabase(config *Config) *gorm.DB {
-	// Menyiapkan koneksi ke database MySQL
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.DBUser, config.DBPassword, config.DBHost, config.DBPort, config.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to the database:", err)
-	}
-	log.Println("Successfully connected to the database")
-
-	// Melakukan AutoMigrate untuk model yang ada
-	err = db.AutoMigrate(
-		&model.Event{},
-		&model.Ticket{},
-		&model.User{},
-	)
-
-	if err != nil {
-		log.Fatal("Failed to migrate database:", err)
-	}
-
-	log.Println("Database migrated successfully")
-	return db
 }
